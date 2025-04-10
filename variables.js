c7f77f1503f4e8fd699181b57a314db77e57bc2d
Thandeka215 <146929727+Thandeka215@users.noreply.github.com>
2025-04-10 18:24:15 +0200

    Add files via upload

---
+++ variables.js
@@ -0,0 +1,83 @@
+let name = "Thandeka";
+let age = 22;
+let isStudent = false;
+
+console.log("name: ", "name");
+console.log("age: ", "age");
+console.log("is Student:".isStudent);
+
+//excercise 2
+let favoriteColour = "Orange";
+console.log("favoriteColour: ", "orange");
+
+//excercise 3
+let num1 = 22;
+let num2 = 3;
+
+let sum = num1 + num2;
+let difference = num1 - num2;
+let product = num1 * num2;
+let quotient = num1 / num2;
+
+console.log("sum: ", "sum");
+console.log("difference: ", "difference");
+console.log("product: ", "product");
+console.log("quotient: ", "quotient");
+
+//data types and operators
+//excercise 1
+let num = 10;
+let float = 24.6;
+
+let addition = num + 2;
+let subtraction = num - 3;
+let multiplication = num * 2;
+let division = num / 2;
+let modulus = num % 100;
+let expo = num ** 2;
+
+console.log("num: ", "num");
+console.log("float: ", "float");
+console.log("addition: ", "addition");
+console.log("subtraction: ", "subtraction");
+console.log("multiplication: ", "multiplication");
+console.log("division: ", "division");
+console.log("modulus: ", "modulus");
+console.log("expo: ", "expo");
+
+let x = 8;
+let y = 12;
+
+let isGreater = x > y;
+let isLessOrEqual = x <= y;
+let isEqual = x === y;
+let isNotEqual = x !== y;
+
+console.log("x > y: ", "isGreater");
+console.log("x <= y: ", "isLessOrEqual");
+console.log("x === y: ", "isEqual");
+console.log("x !== y: ", "isNotEqual");
+
+let a = true;
+let b = false;
+
+console.log("a AND b: ", a && b);
+console.log("a || b: ", a || b);
+console.log("NOT a: ", !a);
+
+let p = 10;
+
+p += 5;
+console.log("result +=5: ", +p);
+
+p -= 3;
+console.log("result -=2: ", +p);
+
+p *= 4;
+console.log("result *=4: ", +p);
+
+p /= 6;
+console.log("result /=6:", +p);
+
+p %= 100;
+console.log("result %= 100: ", +p);
